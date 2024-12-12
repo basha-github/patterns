51ebfd02c8f505c46aad2caa729ff4906467a42c
sharifabee <sharifabee.alur@gmail.com>
2024-12-12 11:32:02 +0530

    v1-p6

--- Pattern4.java
+++ Pattern4.java
@@ -1,8 +1,5 @@
-
 public class Pattern4 {
-
 	public static void main(String[] args) {
-		
 		/*
 		 * 	***************************
 		 *  *                         *
@@ -13,14 +10,9 @@ public class Pattern4 {
 		 *  *                         *
 		 *  *                         *
 		 *  *************************** 
-		 * 
 		 */
-
 		for(int rows=1;rows<=20;rows++) {
-			
-			
 			System.out.print("\t");
-			
 			if(rows == 1 || rows == 20) {
 				for(int cols=1;cols<=35;cols++) {
 					System.out.print("*");
